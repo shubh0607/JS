9bf82dea653de4e18e442796476472637e245579
Shubham Bhimraj Jadhav <167076591+shubh0607@users.noreply.github.com>
2024-08-06 19:50:32 +0530

    Add files via upload

---
+++ 02_dataType.js
@@ -0,0 +1,29 @@
+"use strict"; // treat all JS code as newer version 
+
+let name = "Shubhamm"
+let state = null 
+
+//number => 2 to power 53
+// bigint
+// string =>""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol find unique 
+
+// object
+
+console.log(typeof "Shubham" )
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ test.js
@@ -0,0 +1,9 @@
+console.log("Shubham")
+const accountId = 12232
+let accountEmail = "shubh@gmail.com"
+var accountPassword="1234"
+accountCity = "Jaipur"
+
+//accountId = 2
+// note 
+console.log([accountId, accountEmail, accountPassword])
