4c4e5749566f8c7e15cf022fed774dcf43d86d1b
Shubham Bhimraj Jadhav <167076591+shubh0607@users.noreply.github.com>
2024-08-09 15:35:56 +0000

    stack and heap

---
+++ stack_and_heap.js
@@ -0,0 +1,21 @@
+// Stack memory use in (primitive)
+// Heap memory use in (Non-Primitive)
+
+let myYoutubename = "SHubhamjadhav"
+let anothername = myYoutubename
+anothername ="ShubhYT"
+
+console.log(anothername);
+console.log(anothername);
+
+let userOne ={
+    email: "user@google.com",
+    upi: "user@ybl"
+
+}
+let userTwo = userOne
+
+userTwo.email = "shubham@gmail.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
